f53ef72a2db38f12f8f5fa2187843384652dce59
Abdul <ahakimmaulana07@gmail.com>
2026-01-13 22:23:05 +0700

    routing & database

--- backend_absensi_api/main.go
+++ backend_absensi_api/main.go
@@ -13,11 +13,11 @@ import (
 func main() {
 	app := fiber.New()                                   // menjalankan fungsi app fiber
 	config.DatabaseConnection()                          // Koneksi ke database
+	app.Use(cors.New(cors.Config{AllowOrigins: "*"}))    // Mengizinkan semua origin untuk akses API
 	app.Post("/login", controllers.LoginOrRegister)      // Untuk halaman login
 	app.Post("/absen", controllers.SubmitAbsen)          // Untuk fitur 1, absensi
 	app.Get("/user/:id", controllers.GetUserData)        // Pemilihan user
 	app.Get("/histori/:user_id", controllers.GetHistori) // Untuk fitur 2, histori
-	app.Use(cors.New(cors.Config{AllowOrigins: "*"}))    // Mengizinkan semua origin untuk akses API
 	port := os.Getenv("PORT")                            // Mengambil port dari import os
 	if port == "" {
 		port = "3000" // Default port jika dilakukannya fallback
